e2b6cc6ac1bcfd89ec353bfbebb217403a2dab36
Markus Schwarz <blek@gmx.net>
2020-10-27 08:38:42 +0100

    load eingebaut

--- README.md
+++ README.md
@@ -14,8 +14,20 @@ group1.add_channel("sin", np.sin(x))
 group1.add_channel("cos", np.cos(x), unit='mm', color='#202020')
 doc.save("lalelu.xlog")
 ```
+
 ## load varlog data into python
 
+```python
+import pyindel
+import matplotlib.pyplot as plt
 
+doc = pyindel.load_xlog('myfile.xlog')
+x_cmds = doc.group("X").channel("cmdS").data
+y_cmds = doc.group("Y").channel("cmdS").data
+z_cmds = doc.group("Z").channel("cmdS").data
 
-
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+ax.plot3D(x_cmds, y_cmds, z_cmds, 'gray')
+plt.show()
+```
--- example_load.py
+++ example_load.py
@@ -1,13 +1,23 @@
 import pyindel
 import os
+import matplotlib.pyplot as plt
 
 curdir = os.path.dirname(os.path.abspath(__file__))
+
 fn = os.path.join(curdir, './pyindel/realcase.xlog')
 doc = pyindel.load_xlog(fn)
+x_cmds = doc.group("X").channel("cmdS").data
+y_cmds = doc.group("Y").channel("cmdS").data
+z_cmds = doc.group("Z").channel("cmdS").data
 
-x_cmds =  doc.group("X").channel("cmdS")
-y_cmds =  doc.group("Y").channel("cmdS")
-z_cmds =  doc.group("Z").channel("cmdS")
+# strip curve
+begin = 2000
+end = 5000
+x_cmds = x_cmds[begin:end]
+y_cmds = y_cmds[begin:end]
+z_cmds = z_cmds[begin:end]
 
-print(z_cmds)
-
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+ax.plot3D(x_cmds, y_cmds, z_cmds, 'gray')
+plt.show()
